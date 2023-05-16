cb76e0290bf699d46b25d23b00cb33514c5b4f7a
kwakkyeongrok <kyeongrok@naver.com>
2023-05-16 22:23:46 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+import ImagesUpload from 'react-images-upload';
+
 
 function App() {
+
+  const [images, setImages] = useState([]);
+
+  const onDrop = (files) => {
+    setImages([...images, ...files]);
+  };
+  const removeImage = (index) => {
+    const newImages = [...images];
+    newImages.splice(index, 1);
+    setImages(newImages);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dropzone onDrop={onDrop}>
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()} className="dropzone">
+            <input {...getInputProps()} />
+            <p>이미지를 업로드하려면 클릭하거나 여기에 이미지를 끌어다 놓으세요.</p>
+          </div>
+        )}
+      </Dropzone>
+      <ImagesUpload
+        className="images-preview"
+        onChange={onDrop}
+        imgExtension={['.jpg', '.png', '.gif']}
+        maxFileSize={5242880}
+        withPreview
+      />
+      <div className="image-list">
+        {images.map((image, index) => (
+          <div key={index} className="image-item">
+            <img src={URL.createObjectURL(image)} alt="uploaded" />
+            <button onClick={() => removeImage(index)}>Remove</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
